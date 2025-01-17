e5b892349056143e8a3d116283a11255a2e7ba08
TYHTTT3850 <Tyh11811@outlook.com>
2025-01-17 21:08:42 +0800

    Update mywidget.h

--- Qt6学习代码/The First Qt6 Program/UiFile/mywidget.h
+++ Qt6学习代码/The First Qt6 Program/UiFile/mywidget.h
@@ -1,27 +1,23 @@
-//
-// Created by Tyh11 on 25-1-17.
-//
-
 #ifndef MYWIDGET_H
 #define MYWIDGET_H
 
 #include <QWidget> //包含 QWidget 窗口类头文件
 
-//声明一个名字空间，包含一个类 myWidget。这个类不是此文件中定义的，而是在ui_mywidget.h文件中定义的的。
+//声明一个名字空间，包含一个类 myWidget 。这个类不是此文件中定义的，而是在 ui_mywidget.h 文件中定义的。
 QT_BEGIN_NAMESPACE
 namespace Ui { class myWidget; }
 QT_END_NAMESPACE
 
 class myWidget : public QWidget {
-Q_OBJECT //Q_Object宏，允许类使用信号和槽的机制
+Q_OBJECT // Q_Object 宏，允许类使用信号和槽的机制
 
 public:
     explicit myWidget(QWidget *parent = nullptr);
     ~myWidget() override;
 
 private:
-    //称Ui::myWidget为窗口Ui类
-    Ui::myWidget *ui; //窗口Ui类指针，指向可视化设计的窗口界面，要访问界面上的组件，就需要通过该指针实现
+    //称 Ui::myWidget 为窗口Ui类
+    Ui::myWidget *ui; //窗口 Ui 类指针，指向可视化设计的窗口界面，要访问界面上的组件，就需要通过该指针实现
 };
 
 
