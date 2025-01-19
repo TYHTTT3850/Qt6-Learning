4ce45febe72b00ccab290803c86215cd3157bb57
TYHTTT3850 <Tyh11811@outlook.com>
2025-01-19 21:54:56 +0800

    Delete Qt6学习代码/FirstAttempt directory

--- Qt6学习代码/FirstAttempt/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "UiFile/widget.h"
-#include "UiFile/ui_widget.h"
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return QApplication::exec();
-}
