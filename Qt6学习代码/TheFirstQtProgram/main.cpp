e2b37941af5f31a24ea0872044853dddad89396a
TYHTTT3850 <Tyh11811@outlook.com>
2025-01-20 00:24:57 +0800

    更新 main.cpp

--- Qt6学习代码/TheFirstQtProgram/main.cpp
+++ Qt6学习代码/TheFirstQtProgram/main.cpp
@@ -3,7 +3,12 @@
 #include "UiFile/ui_Widget.h"
 
 //文件ui_widget.h 里封装界面的类是Ui_Widget。
-//再定义一个类Widget 从Ui_Widget 继承而来，并将其定义在名字空间 Ui 里，这样 Ui::Widget 与文件 widget.h 里的类 Widget 同名，但是用名字空间区分开。
+
+//再定义一个类 Widget 从 Ui_Widget 继承而来，并将其定义在名字空间 Ui 里，这样 Ui::Widget 与文件 widget.h 里的类 Widget 同名，但是用名字空间区分开。
+
+//在文件 ui_widget.h 里有一个名字空间Ui，里面有一个类Widget，记作 Ui::Widget，它是窗口 UI 类。
+
+//文件 widget.h 里的类 Widget 是完整的窗口类。
 
 //main：程序的入口。argc：命令行变量的数量。argv：命令行变量的数组。
 int main(int argc, char *argv[]) {
