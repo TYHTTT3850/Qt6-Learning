b2b1b58d93ff810e909759b4a2f901d5d82ba453
TYHTTT3850 <Tyh11811@outlook.com>
2025-01-20 22:31:00 +0800

    Update Widget.cpp

--- Qt6学习代码/信号与槽/Multiple_Signal_Sender/UiFile/Widget.cpp
+++ Qt6学习代码/信号与槽/Multiple_Signal_Sender/UiFile/Widget.cpp
@@ -5,6 +5,7 @@
 Widget::Widget(QWidget *parent) :
     QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
+    //连接多个信号到同一个槽函数
     connect(ui->sender_button1, SIGNAL(clicked()), this, SLOT(getTheSignal()));
     connect(ui->sender_button2, SIGNAL(clicked()), this, SLOT(getTheSignal()));
     connect(ui->sender_button3, SIGNAL(clicked()), this, SLOT(getTheSignal()));
